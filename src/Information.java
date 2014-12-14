1611e5cfb04abe2c8c5bcf9a3e4e002e683f46f9
aurelien.tamasle@gmail.com <aurelien.tamasle@gmail.com@2b50e9ea-8156-3f91-bff2-627c13eef279>
2014-12-14 23:18:23 +0000

    Ajout de javamail, Création de la première version de la classe Information

---
+++ src/Information.java
@@ -0,0 +1,15 @@
+import javax.mail.internet.InternetAddress;
+
+
+public class Information {
+	private String name;
+	private String surname;
+	private String password;
+	private InternetAddress email;
+	private int[] phoneNumber;
+	private String workplace;
+	private String home;
+	private boolean driver;
+	private boolean notifications;
+	
+}

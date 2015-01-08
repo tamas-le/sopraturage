0d88c6a828782485f608ae0728516f0cf5de0b70
julien.baladier@gmail.com <julien.baladier@gmail.com@2b50e9ea-8156-3f91-bff2-627c13eef279>
2015-01-08 19:17:20 +0000

    requests update for new version

--- Database/requests.sql
+++ Database/requests.sql
@@ -4,10 +4,10 @@ INSERT INTO Postcodes (postcode, city)
 			('30100', 'Alès');
 
 
-INSERT INTO Addresses (num, way_type, way_name, id_postcode)
-	 VALUES ('17', 'avenue', "Colonel Roche", (SELECT id FROM Postcodes WHERE postcode = "31400")),
-	 		('37', 'chemin', "Ramassiers", (SELECT id FROM Postcodes WHERE postcode = "31770")),
-	 		('378', 'chemin', "Espinaux à la bedosse", (SELECT id FROM Postcodes WHERE postcode = "30100"));
+INSERT INTO Addresses (num, way_type, way_name, id_postcode, longitude, latitude)
+	 VALUES ('17', 'avenue', "Colonel Roche", (SELECT id FROM Postcodes WHERE postcode = "31400"), '37.4224411','-122.0842864'),
+	 		('37', 'chemin', "Ramassiers", (SELECT id FROM Postcodes WHERE postcode = "31770"), '37.4234411','-122.0842864'),
+	 		('378', 'chemin', "Espinaux à la bedosse", (SELECT id FROM Postcodes WHERE postcode = "30100"), '37.4227411','-122.0842864');
 
 
 
@@ -39,6 +39,11 @@ INSERT INTO Users (first_name, last_name, email, password, phone_number, workpla
 
 INSERT INTO Administrators
 	 VALUES (1);
+
+INSERT INTO Sessions (id, time_stamp_connection, time_stamp_deconnection)
+	 VALUES (1, "2000-01-01 00:00:01", "2000-01-01 00:00:01");
+
+
 
 
 -- Récuperer toutes les workplaces

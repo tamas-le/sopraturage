742e60c770c213ba9eabd77c20b44e8f14957ea0
julien.baladier@gmail.com <julien.baladier@gmail.com@2b50e9ea-8156-3f91-bff2-627c13eef279>
2015-01-11 00:53:21 +0000

    Création d'une classe Time pour convertir une durée en seconde en String

---
+++ src/sopraturage/util/Time.java
@@ -0,0 +1,25 @@
+package sopraturage.util;
+
+public class Time {
+	
+	
+	public static String convertToTime(long longVal){
+	    int hours = (int) longVal / 3600;
+	    int remainder = (int) longVal - hours * 3600;
+	    int mins = remainder / 60;
+	    remainder = remainder - mins * 60;
+	    int secs = remainder;
+	    
+	    return hours + " hours " + mins + " minutes " + secs + " seconds";
+	}
+	
+	/*
+	public static void main(String[] args) {
+		
+		long valeur = 43534;
+		System.out.println(convertToTime(valeur));
+		
+	}
+	*/
+	
+}
